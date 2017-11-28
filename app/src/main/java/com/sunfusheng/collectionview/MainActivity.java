0bde4bb53eb10164877f0c7b77216d261d426fb7
sfsheng0322 <sfsheng0322@gmail.com>
2017-11-28 15:38:40 +0800

    vo

--- app/src/main/java/com/sunfusheng/collectionview/MainActivity.java
+++ app/src/main/java/com/sunfusheng/collectionview/MainActivity.java
@@ -2,12 +2,18 @@ package com.sunfusheng.collectionview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
     }
 }
